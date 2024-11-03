24bb1ab9b95b95da843bb7a4169b62ff8a5f8e9f
adiii3692 <adinair0206@gmail.com>
2024-11-03 15:22:10 -0500

    Lists in csharp are just like vectors in c++

--- Program.cs
+++ Program.cs
@@ -1,28 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Collections.Generic;
 
 namespace Program{
     class Program{
         public static void Main(string[] args){
-            //Data Types and type conversion
-            // double pi = 3.14;
-            // int int_pi = Convert.ToInt32(pi);
-            // string str_pi = Convert.ToString(pi);
-            // Console.WriteLine("PI as an integer is: "+ int_pi + int_pi.GetType());
-            // Console.WriteLine("Pi as a string is: "+str_pi.GetType());
-
-
-            //User input
-        //     Console.WriteLine("Enter your name: ");
-        //     string name = Console.ReadLine();
-        //     Console.WriteLine("Hello, "+name);
-
-            //Arrays
-            double[] tickets;
-            for(int i=0;i<tickets.length;i++){
-                tickets[i] = i * i;
-                Console.WriteLine(tickets[i]);
-            }
+            //A List of integers
+            List<int> firstList = new List<int>();
          }
     }
 }
